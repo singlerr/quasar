72fc2f391f4d0119206f7ab44a092b7a5cf4e8a3
Singlerr <singlerr@soongsil.ac.kr>
2022-11-28 17:28:23 +0900

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quasar
+
+-----
+
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/io.github.singlerr/quasar/badge.svg)](https://maven-badges.herokuapp.com/maven-central/io.github.singlerr/quasar)
---
+++ src/main/java/io/github/singlerr/quasar/entity/CompatibleEntity.java
@@ -0,0 +1,11 @@
+package io.github.singlerr.quasar.entity;
+
+import io.github.singlerr.quasar.Compatible;
+import org.bukkit.entity.Entity;
+
+/***
+ * Compatible class for {@link org.bukkit.entity.Entity}
+ * @author Singlerr
+ */
+public interface CompatibleEntity extends Entity, Compatible {
+}
